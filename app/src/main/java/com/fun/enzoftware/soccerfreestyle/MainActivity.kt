47862250a012bd0e68622366a71fff4baddd5b74
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-09-18 02:36:10 -0500

    beauty animation

--- app/src/main/java/com/fun/enzoftware/soccerfreestyle/MainActivity.kt
+++ app/src/main/java/com/fun/enzoftware/soccerfreestyle/MainActivity.kt
@@ -2,8 +2,11 @@ package com.`fun`.enzoftware.soccerfreestyle
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.TranslateAnimation
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     var counterKicks = 0
@@ -11,11 +14,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ball.setOnClickListener {
+
+
+            ball.setOnClickListener {
             counterKicks++
             counter.text = counterKicks.toString()
-            ball.y = 250.0f
-            ball.x = 200.0f
+            val animation = TranslateAnimation(0.0f, 0.0f, 0.0f, -750.0f)
+            animation.duration = 700
+            animation.repeatCount = 1
+            animation.repeatMode = 2
+            animation.fillAfter = true
+            ball.startAnimation(animation)
         }
     }
+
+
+
 }
