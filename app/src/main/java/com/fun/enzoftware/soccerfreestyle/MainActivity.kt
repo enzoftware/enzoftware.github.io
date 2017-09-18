80abffd16bec05115fc4f7bffb276847cd0789d4
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-09-18 01:50:16 -0500

    change ball position

--- app/src/main/java/com/fun/enzoftware/soccerfreestyle/MainActivity.kt
+++ app/src/main/java/com/fun/enzoftware/soccerfreestyle/MainActivity.kt
@@ -7,12 +7,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var counterKicks = 0
+    var incrementer = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ball.setOnClickListener {
             counterKicks++
             counter.text = counterKicks.toString()
+            ball.y = 250.0f
+            ball.x = 200.0f
         }
     }
 }
