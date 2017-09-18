6cb3610a8aca71e32597ddae57d20c351865f3f3
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-09-18 01:12:50 -0500

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/fun/enzoftware/soccerfreestyle/MainActivity.kt
+++ app/src/main/java/com/fun/enzoftware/soccerfreestyle/MainActivity.kt
@@ -2,11 +2,16 @@ package com.`fun`.enzoftware.soccerfreestyle
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ball.setOnClickListener {
+            Toast.makeText(this,"hola",Toast.LENGTH_SHORT).show()
+        }
     }
 }
