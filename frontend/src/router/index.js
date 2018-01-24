4e0e515160854617ec8e36ae59a3a41d5008f667
enzoftware <lizama.enzo@gmail.com>
2018-01-24 18:16:02 -0500

    Setup and routes , done

---
+++ frontend/src/router/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable semi */
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const routerOptions = [
+  {path: '/', component: 'Home'},
+  {path: '/about', component: 'About'}
+];
+
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    // eslint-disable-next-line no-template-curly-in-string
+    component: () => {
+      return import(`@/components/${route.component}.vue`);
+    }
+  }
+});
+
+Vue.use(Router);
+
+export default new Router({
+  routes,
+  mode: 'history'
+})
