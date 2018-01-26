433678321458f40812180dc8a545aec74a72dc04
enzoftware <lizama.enzo@gmail.com>
2018-01-26 11:42:45 -0500

    Setup flask

---
+++ dist/static/js/2.840794e066a1055ce817.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{c27y:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n  About\n")])},staticRenderFns:[]},r=t("VU/8")(null,l,!1,null,null,null);n.default=r.exports}});
+//# sourceMappingURL=2.840794e066a1055ce817.js.map
