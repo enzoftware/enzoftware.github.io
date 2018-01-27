8896648e547bad0a12e6fe4a41f870fe08137dee
enzoftware <lizama.enzo@gmail.com>
2018-01-26 21:40:34 -0500

    random message

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ app = Flask(	__name__,
 		static_folder = "./dist/static",
 		template_folder = "./dist")
 
-@app.route('/')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+	return render_template("index.html")
+
 def index():
 	return render_template("index.html")
