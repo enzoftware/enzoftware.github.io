11e5b99cbd300754175c9f2833e17be6e7f0caf4
enzoftware <lizama.enzo@gmail.com>
2018-02-10 23:33:46 -0500

    Initial vue setup

--- dist/static/js/1.5154446f8a8e4a0732f4.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{lO7g:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n  Home\n")])},staticRenderFns:[]},r=t("VU/8")(null,l,!1,null,null,null);n.default=r.exports}});
-//# sourceMappingURL=1.5154446f8a8e4a0732f4.js.map
--- frontend/src/components/NotFound.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1> 404 NOT FOUND </h1>
-</template>
